c1a1eb6523d4c3cbe07caab7f739800c8921f107
BurakKurt <krtburak@outlook.com>
2023-07-02 02:16:01 +0300

    add notes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,24 +7,35 @@ export default function Welcome() {
                 <li>
                     Notes
                     <ul className="ms-10 my-3" style={{listStyleType: "number"}}>
-                        <li>Lazy loader için react loader sekeleton yüklenecek</li>
+                        <li className="line-through decoration-1 decoration-red-400">React sekeleton loader
+                            eklenecek
+                        </li>
+                        <li>React lazy loader eklenecek (Players ve Games sayfaları için sayfa başına fazla veri
+                            yüklendiği zaman olası gecikmeleri önleyecek.)
+                        </li>
+                        <li>Kayıt ol ve Giriş yap sayfası eklenecek, kayıt olunan veriler local storage 'de
+                            saklanacak.
+                        </li>
+                        <li>Giriş yapan kullanıcı belirli bir süre sonra otomatik çıkış yapacak.</li>
+                        <li>Kayıt ol ve giriş yap için reCaptcha eklenecek.</li>
+                        <li>(Authentication - PrivateUser) Takım istatistiklerini sadece giriş yapmış kullanıcılar
+                            görebilecek.
+                        </li>
+                        <li>(Authentication - PrivateAdmin) Sadece adminin erişebileceği bir sayfa tasarlanacak.</li>
+                        <li>Responsive tasarım fixlemeleri yapılacak.</li>
                     </ul>
                 </li>
                 <li>
                     Player Page
                     <ul className="ms-10 my-3" style={{listStyleType: "number"}}>
                         <li>En üste bir search bar olacak ve oyuncular isimlerine göre aranabilecek</li>
-                        <li>Açılışta ortalama 25 oyuncu listelencek</li>
-                        <li>prev next butonları ile diğer oyuncular görülebilecek</li>
+                        <li>Filtreleme Games Page ile aynı olacak.</li>
                     </ul>
                 </li>
                 <li>
                     Home Page
                     <ul className="ms-10 my-3" style={{listStyleType: "number"}}>
-                        <li>Güzel bir görsel tasarım (resimler) eklenecek.</li>
-                        <li>Vip sayfa yapıalcak ve istatistikler daha ayrıntılı gösteriecek, sadece kayıtlı kullanıcılar
-                            görebilir.
-                        </li>
+                        <li>HTML5 video etiketi ile basketbol videosu eklenecek. (background)</li>
                     </ul>
                 </li>
             </ul>
